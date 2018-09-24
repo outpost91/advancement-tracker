d4f88108ed3b14333483b1af2bfbf97c2a3439d6
Adam Wieseler <6730240+outpost91@users.noreply.github.com>
2018-09-24 15:20:57 -0500

    Update adv-ranger-groups.tsx
    Refactored tabs interface

--- src/components/adv-ranger-groups/adv-ranger-groups.tsx
+++ src/components/adv-ranger-groups/adv-ranger-groups.tsx
@@ -1,23 +1,52 @@
-import { Component, Prop } from '@stencil/core';
-
+import { Component, Element, Listen, Prop } from '@stencil/core';
+import { RouterHistory } from '@stencil/router';
 import { AuthService } from '../../services/auth';
-import { DatabaseService } from '../../services/Firestore';
+import { DatabaseService } from '../../services/database';
 
 @Component({
   tag: 'adv-ranger-groups',
   styleUrl: 'adv-ranger-groups.scss'
 })
 export class AdvRangerGroups {
+  _cnt: number;
+  _title: string;
+
+  @Element() el: HTMLAdvRangerGroupsElement;
+
   @Prop() auth: AuthService;
   @Prop() db?: DatabaseService;
+  @Prop() history: RouterHistory;
+
+  @Listen('ionTabbarClick')
+  async onTabBarClicked(event: CustomEvent<HTMLIonTabElement>) {
+    /*
+    ** Work around for tabs when not using ion-router
+    ** in order for props to be passed.
+    */
+    const tabs = await this.el.querySelector('ion-tabs');
+    await tabs.select(event.detail);
+    const tab = await tabs.getSelected();
+    if (tab.href !== undefined) {
+      this.history.push(tab.href, {});
+    }
+  }
+
+  async componentDidLoad() {
+    const tabs = this.el.querySelector('ion-tabs');
+    const tab = tabs.querySelector('ion-tab');
+    await tabs.select(tab);
+    if (tab.href !== undefined) {
+      this.history.push(tab.href, {});
+    }
+  }
 
   render() {
     return (
-      <ion-tabs>
-        <adv-group-tab kids></adv-group-tab>
-        <adv-group-tab discovery></adv-group-tab>
-        <adv-group-tab adventure></adv-group-tab>
-        <adv-group-tab expedition></adv-group-tab>
+      <ion-tabs useRouter>
+        <ion-tab label="Ranger Kids" icon="home" href="/advancement/kids"></ion-tab>
+        <ion-tab label="Discovery" icon="rocket" href="/advancement/discovery"></ion-tab>
+        <ion-tab label="Adventure" icon="trophy" href="/advancement/adventure"></ion-tab>
+        <ion-tab label="Expedition" icon="logo-android" href="/advancement/expedition"></ion-tab>
       </ion-tabs>
     );
   }
